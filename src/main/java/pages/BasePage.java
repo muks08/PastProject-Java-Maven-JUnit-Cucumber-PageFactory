5b4d03a34e66efa82d2b493684a5cc5a4e96c54a
Muks <muks.bodya@gmail.com>
2022-09-21 13:53:02 +0300

    update

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,7 +2,9 @@ package pages;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -16,5 +18,16 @@ public class BasePage {
     public void waitForPageLoadComplete(long timeToWait) {
         new WebDriverWait(driver, timeToWait).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public void waitForAjaxComplete(long timeToWait){
+        new WebDriverWait(driver, timeToWait).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript(
+                        "return window.jQuery != undefined && jQuery.active == 0;"));
+    }
+
+    public void waitVisibilityOfElement(long timeToWait, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, timeToWait);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
