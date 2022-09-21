0228ce04ae38fe8e0fba36bdc6664fb97cd23664
Muks <muks.bodya@gmail.com>
2022-09-21 15:17:48 +0300

    merge from muksbranch

---
+++ src/main/java/manager/PageFactoryManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import pages.*;
+
+public class PageFactoryManager {
+    WebDriver driver;
+
+    public PageFactoryManager(WebDriver driver) {this.driver = driver;}
+
+    public HomePage getHomePage(){return new HomePage(driver);}
+
+    public AllMenShoesPage getAllMenShoesPage(){return new AllMenShoesPage(driver);}
+}
